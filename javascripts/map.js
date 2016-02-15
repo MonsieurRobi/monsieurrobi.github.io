f4197d95d20e6d20f4ae7a686fd121cffb345886
Christophe Robillard <geek@robiweb.net>
2016-02-15 15:26:03 +0100

    reorder events + update map center

--- javascripts/map.js
+++ javascripts/map.js
@@ -1,18 +1,19 @@
 var events = [
-  { "host": 'Fred', "when":"Date à préciser", where: "L'Ile-Saint-Denis (93)", "latitude": 48.90385, longitude: 2.34122},
-  { "host": 'Dominique', "when":"Date à préciser", where: 'Paris', "latitude": 48.85888, "longitude": 2.34694},
   { "host": 'Karine', "when":"30/04/2016", where: "Bergerac", "latitude": 44.85198, longitude: "0.48826"},
-  { "host": 'Karina', "when": "12/05/2016", where: 'Montreuil, 93', "latitude": 48.8623357, "longitude": 2.4412184 },
-  { "host": 'Vincent', "when": "04/06/2016", where: 'Limours, 91', "latitude": 48.6455882, "longitude": 2.075826 },
-  { "host": 'Gaelle', "when": "05/06/2016", where: 'Lagny sur Marne, 77', "latitude": 48.8735584, "longitude": 2.7121663 },
+  { "host": 'Karina', "when": "14/05/2016", where: 'Montreuil, 93', "latitude": 48.8623357, "longitude": 2.4412184 },
   { "host": 'Tyfen', "when": "21/05/2016", where: 'Lucey, Savoie', "latitude": 45.7516, "longitude":5.78737},
-  { "host": 'Rachel', "when": "Date à préciser", where: 'Nancy', "latitude": 48.68807,"longitude":6.17346},
-{ "host": 'Corinne', "when": "11/06/2016", where: 'Diois', "latitude": 44.68101845,"longitude":5.47397281258444},
   { "host": 'Julie', "when": "28/05/2016", where: 'Avignon', "latitude": 43.94172, "longitude":4.83339},
+  { "host": 'Vincent', "when": "04/06/2016", where: 'Limours, 91', "latitude": 48.6455882, "longitude": 2.075826 },
+{ "host": 'Corinne', "when": "11/06/2016", where: 'Diois', "latitude": 44.68101845,"longitude":5.47397281258444},
   { "host": 'Les Sidebottom', "when": "02/07/2016", where: 'Warrington, England', "latitude": 53.40170, "longitude":-2.56155},
+  { "host": 'Rachel', "when": "Date à préciser", where: 'Nancy', "latitude": 48.68807,"longitude":6.17346},
+  { "host": 'Dominique', "when":"Date à préciser", where: 'Paris', "latitude": 48.85888, "longitude": 2.34694},
+  { "host": 'Fred', "when":"Date à préciser", where: "L'Ile-Saint-Denis (93)", "latitude": 48.90385, longitude: 2.34122},
+  { "host": 'Gaelle', "when": "05/06/2016", where: 'Lagny sur Marne, 77', "latitude": 48.8735584, "longitude": 2.7121663 },
+
 ];
 var accessToken = "pk.eyJ1Ijoia3JpY2h0b2YiLCJhIjoiYjU1MTg4MWU5ZDRkNWVkOGRlZjUzZWFjNWYyMWI5YzcifQ.57puNCxCdXweviIgoWBG4Q";
-var origin = [46.603354,1.8883335];
+var origin = [46.603354,-7.8883335];
 var initialZoom = 5;
 
 var map = L.map('map', {
