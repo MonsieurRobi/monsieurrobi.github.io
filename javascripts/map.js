3bdea3709debd8a093470131ac4b95825df38f3c
Christophe Robillard <geek@robiweb.net>
2016-02-17 15:22:39 +0100

    add Nathalie and update Dom

--- javascripts/map.js
+++ javascripts/map.js
@@ -1,14 +1,14 @@
 var events = [
+  { "host": 'Dominique', "when":"02/04/2016", where: 'Paris', "latitude": 48.82615, "longitude": 2.34091},
   { "host": 'Karine', "when":"30/04/2016", where: "Bergerac", "latitude": 44.85198, longitude: "0.48826"},
   { "host": 'Karina', "when": "14/05/2016", where: 'Montreuil, 93', "latitude": 48.8623357, "longitude": 2.4412184 },
   { "host": 'Tyfen', "when": "21/05/2016", where: 'Lucey, Savoie', "latitude": 45.7516, "longitude":5.78737},
   { "host": 'Julie', "when": "28/05/2016", where: 'Avignon', "latitude": 43.94172, "longitude":4.83339},
   { "host": 'Vincent', "when": "04/06/2016", where: 'Limours, 91', "latitude": 48.6455882, "longitude": 2.075826 },
 { "host": 'Corinne', "when": "11/06/2016", where: 'Diois', "latitude": 44.68101845,"longitude":5.47397281258444},
-  { "host": 'Les Sidebottom', "when": "02/07/2016", where: 'Warrington, England', "latitude": 53.40170, "longitude":-2.56155},
   { "host": 'Rachel', "when": "25/06/2016", where: 'Nancy', "latitude": 48.68807,"longitude":6.17346},
-  { "host": 'Dominique', "when":"Date à préciser", where: 'Paris', "latitude": 48.85888, "longitude": 2.34694},
-  { "host": 'Fred', "when":"Date à préciser", where: "L'Ile-Saint-Denis (93)", "latitude": 48.90385, longitude: 2.34122},
+  { "host": 'Les Sidebottom', "when": "02/07/2016", where: 'Warrington, England', "latitude": 53.40170, "longitude":-2.56155},
+  { "host": 'Nathalie', "when":"09/07/2016", where: "Paris", "latitude": 48.88481, "longitude": 2.30284},
   { "host": 'Gaelle', "when": "05/06/2016", where: 'Lagny sur Marne, 77', "latitude": 48.8735584, "longitude": 2.7121663 },
 
 ];
